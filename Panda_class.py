57b44c40bf76ea4cdfcc52b89983a3a4fa3d2bb6
nkolchakov <nikola.kolchakov@gmail.com>
2016-01-06 20:18:56 +0200

    add panda class

---
+++ Panda_class.py
@@ -0,0 +1,46 @@
+import re
+
+
+class Panda:
+
+    def __init__(self, name, email, gender):
+        self._name = name
+        self._email = email
+        if not self.is_mail_valid():
+            raise Exception('Invalid mail !')
+        self._gender = gender
+
+    def get_name(self):
+        return self._name
+
+    def get_email(self):
+        return self._email
+
+    def get_gender(self):
+        return self._gender
+
+    def isMale(self):
+        return self._gender == 'male'
+
+    def isFemale(self):
+        return not self.isMale()
+
+    def is_mail_valid(self):
+        if re.match(r"[^@]+@[^@]+\.[^@]+", self.get_email()):
+            return True
+        return False
+
+    def __str__(self):
+        return 'Name>{}, Mail>{}, Gender>{}'.format(
+                self.name, self.email, self.gender)
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self.get_name() == other.get_name() and self.get_email() == other.get_email() and self.get_gender() == other.get_gender()
+
+    def __hash__(self):
+        return hash(str(self))
+
+
