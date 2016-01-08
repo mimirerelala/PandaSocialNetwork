3f9c3874fc7091572f68d3ddbc84fcef067bfa73
mimirerelala <mirela.napetova@gmail.com>
2016-01-08 20:44:49 +0200

    Fied panda unit test :)

---
+++ test_panda_class.py
@@ -0,0 +1,26 @@
+import unittest
+from Panda_class import Panda
+from PandaSocialNetwork import PandaSocialNetwork
+
+
+class TestPandaClass(unittest.TestCase):
+    def setUp(self):
+        self.ivo = Panda("Ivo", "ivo@pandamail.bg", "male")
+
+    def test_get_name(self):
+        self.assertEqual("Ivo", self.ivo.get_name())
+
+    def test_get_email(self):
+        self.assertEqual(self.ivo.get_email(), "ivo@pandamail.bg")
+
+    def test_get_gender(self):
+        self.assertEqual("male", self.ivo.get_gender())
+
+    def test_isMale(self):
+        self.assertTrue(self.ivo.isMale())
+
+    def test_isFemale(self):
+        self.assertFalse(self.ivo.isFemale())
+
+if __name__ == "__main__":
+    unittest.main()
